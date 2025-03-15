18bd7f6dddfee86234f96a0bd9a11cbda405c15a
lithugedevelopment <lithugedevelopment@gmail.com>
2025-03-15 14:39:06 -0400

    cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -10,7 +10,7 @@ function ProductList({ onHomeClick }) {
 
     const dispatch = useDispatch();
 
-    const cartItems = useSelector((state) => state.cart.times);
+    const cartItems = useSelector((state) => state.cart.items);
 
     const plantsArray = [
         {
@@ -284,7 +284,22 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div>
+  <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+    <h1 className='cart'>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" height="68" width="68">
+        <rect width="156" height="156" fill="none"></rect>
+        <circle cx="80" cy="216" r="12"></circle>
+        <circle cx="184" cy="216" r="12"></circle>
+        <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path>
+      </svg>
+      {/* Display item count */}
+      {cartItems.length > 0 && (
+        <span className="cart-count">{cartItems.length}</span>
+      )}
+    </h1>
+  </a>
+</div>
                 </div>
             </div>
             {!showCart ? (
@@ -298,7 +313,9 @@ function ProductList({ onHomeClick }) {
                 <img className="product-image" src={plant.image} alt={plant.name} />
                 <div className="product-title">{plant.name}</div>
                 {/*Similarly like the above plant.name show other details like description and cost*/}
-                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                <button className="product-button" onClick={() => handleAddToCart(plant)}
+                     disabled={addedToCart[plant.name]} // Disable when already added
+                        > {addedToCart[plant.name] ? "Added" : "Add to Cart"} </button>
             </div>
             ))}
         </div>
